843511887c4b5b0ae7870e58df21b4aee94237b0
Luckyhudda <mr.luckyhudda@gmail.com>
2023-10-16 09:08:55 +0530

    update CRUD methods

--- controllers/crudController.js
+++ controllers/crudController.js
@@ -1,19 +1,102 @@
+const reposetory = require("../db/mongodb/repository");
+
 const crudController = {
-     Get:(req,res) =>{
-
-     },
-     GetOne:(req,res) =>{
-
-     },
-     Post:(req,res) =>{
-
-     },
-     Put:(req,res) =>{
-
-     },
-     Delete:(req,res) =>{
-
-     }
+  Get: (req, res) => {
+    reposetory
+      .FindAll()
+      .then((result) => {
+        res.json({
+          message: "success",
+          result,
+        });
+      })
+      .catch((err) => {
+        res.json({
+          message: "failed",
+          error: err,
+        });
+      });
+  },
+  GetOne: (req, res) => {
+    reposetory
+      .FindOne(req.params.id)
+      .then((result) => {
+        res.json({
+          message: "success",
+          result,
+        });
+      })
+      .catch((err) => {
+        res.json({
+          message: "failed",
+          error: err,
+        });
+      });
+  },
+  Post: (req, res) => {
+    reposetory
+      .InsertOne(req.body)
+      .then((result) => {
+        res.json({
+          message: "success",
+          result,
+        });
+      })
+      .catch((err) => {
+        res.json({
+          message: "failed",
+          error: err,
+        });
+      });
+  },
+  Post: (req, res) => {
+    reposetory
+      .InsertMany(req.body)
+      .then((result) => {
+        res.json({
+          message: "success",
+          result,
+        });
+      })
+      .catch((err) => {
+        res.json({
+          message: "failed",
+          error: err,
+        });
+      });
+  },
+  Put: (req, res) => {
+    reposetory
+      .FindOne(req.params.id)
+      .then((result) => {
+        res.json({
+          message: "success",
+          result,
+        });
+      })
+      .catch((err) => {
+        res.json({
+          message: "failed",
+          error: err,
+        });
+      });
+  },
+  Delete: (req, res) => {
+    reposetory
+      .FindOne(req.params.id)
+      .then((result) => {
+        res.json({
+          message: "success",
+          result,
+        });
+      })
+      .catch((err) => {
+        res.json({
+          message: "failed",
+          error: err,
+        });
+      });
+  },
 };
 
 module.exports = crudController;
