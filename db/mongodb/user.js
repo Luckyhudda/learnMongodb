6691da5af720b0e29f5dd6b6889812c85d46665a
Luckyhudda <mr.luckyhudda@gmail.com>
2023-10-18 16:24:37 +0530

    update MODAL

--- db/mongodb/user.js
+++ db/mongodb/user.js
@@ -2,16 +2,21 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  name: String, // String is shorthand for {type: String}
-  email: String,
-//   about: String,
-//   image: String,
-//   username: {
-//     type: String,
-//     unique: true,
-//   },
-  password: String,
-//   date: { type: Date, default: Date.now },
+  name: {
+    type: String,
+    required: true,
+  },
+  age:Number,
+  gender: {
+    type: String,
+    enum: ["Male", "Female", "Other"],
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  }
 });
 const userModel = mongoose.model("user", userSchema);
 module.exports = userModel;
