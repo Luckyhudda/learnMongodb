d5be1bf99587d21464230c1276d8223e014f35db
Luckyhudda <mr.luckyhudda@gmail.com>
2023-10-20 16:03:36 +0530

    unhandled Errors

--- service.js
+++ service.js
@@ -1,16 +1,33 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+
+
+//  unhandled exception occurs in application
+process.on('uncaughtException',(err) =>{
+  console.log(err.name,err.message);
+  process.exit();
+})
+
+
 const app = require("./app");
 
 dotenv.config();
 mongoose
   .connect(process.env.MONGOURL).then(() => {
     console.log('connected to mongodb');
-  }).catch((err) =>{
-    console.log('error' , err)
   })
 
 const port = 8900;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`server started at: ${port}`);
 });
+
+
+//  Promise is rejected, and the rejection is not handled by you
+process.on('unhandledRejection',err =>{
+  console.log(err.name, err.message);
+  server.close(() =>{
+    process.exit(1);
+  })
+})
+
