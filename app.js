e58d603a4c9650d7c6c06b743ca9f1bfcca4e86f
Luckyhudda <mr.luckyhudda@gmail.com>
2023-10-20 14:22:20 +0530

    handle Routes Error

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 app.use(express.json());
 const curdRoute = require("./routes/crud");
 
-app.use("/crud", curdRoute);
+app.use("/students", curdRoute);
+
+app.all('*', (req,res) =>{
+    res.status(404).json({
+        message: 'Page Not Found'
+    })
+})
 
 module.exports = app;
